d2a613c3452a948c46816442118356a6b20a6770
sukmaputrin <sukma.putri@undiksha.ac.id>
2021-04-27 20:12:10 +0700

    virdha

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:api_sukma/main.dart';
+import 'package:api_virdha/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
